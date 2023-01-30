75696cd4b1153860fa2f81e3cafa0f1afca30762
ikramdagci <ikramd@atp.com.tr>
2023-01-30 17:15:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello Node");
+
+var slugify = require('slugify');
+
+const title = slugify('some string');
+
+console.log(title);
+
+const title2 = slugify('some string lorem ipsum','*');
+
+console.log(title2);
