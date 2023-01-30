4adb44855d6bb52e63a7aafed6b9d25b1ff93174
ikramdagci <ikramdgc@gmail.com>
2023-01-30 17:17:54 +0300

    slugify test

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ console.log(title);
 const title2 = slugify('some string lorem ipsum','*');
 
 console.log(title2);
+
+
+const title3 = slugify('some string lorem ipsum','+');
+
+console.log(title3);
